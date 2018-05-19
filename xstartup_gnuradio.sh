0383b295b7f33cc92fa1b21c14131913c7f9102e
astro <m.rahm7n@gmail.com>
2018-05-19 08:37:38 +0200

    xstartup

---
+++ xstartup_gnuradio.sh
@@ -0,0 +1,10 @@
+#The content of this file must be added to xstartup file
+#located at $HOME/.vnc/xstartup 
+#to specify X app (GNURadio) to be run when a VNC
+#desktop  is started for each session. 
+
+#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &
+gnuradio-companion -title "GNURadio:Simulation Server"
+#gnuradio-companion
